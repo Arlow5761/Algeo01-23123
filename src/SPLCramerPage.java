1bdf5e52c7d165312b5551eb5535f343adf7af33
Arlow5761 <arlow5761@gmail.com>
2024-10-23 09:22:41 +0700

    feat: add contents of spl gauss page

--- src/SPLCramerPage.java
+++ src/SPLCramerPage.java
@@ -1,7 +1,13 @@
-public class SPLCramerPage extends SPLPage
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class SPLCramerPage extends Page
 {
     public SPLCramerPage()
     {
-        super("../assets/spl-cramer.png");
+        JLabel image = new JLabel(new ImageIcon("../assets/spl-cramer.png"));
+        image.setBounds(0, 0, 700, 925);
+
+        page.add(image);
     }
 }
--- src/SPLInversePage.java
+++ src/SPLInversePage.java
@@ -1,7 +1,13 @@
-public class SPLInversePage extends SPLPage
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class SPLInversePage extends Page
 {
     public SPLInversePage()
     {
-        super("../assets/spl-inverse.png");
+        JLabel image = new JLabel(new ImageIcon("../assets/spl-inverse.png"));
+        image.setBounds(0, 0, 700, 925);
+
+        page.add(image);
     }
 }
