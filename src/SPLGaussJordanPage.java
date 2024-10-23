0feaac7ff4dd85b7bd0ed3be97370b119b649da6
Arlow5761 <arlow5761@gmail.com>
2024-10-23 09:22:41 +0700

    feat: add contents of spl gauss page

--- src/SPLGaussJordanPage.java
+++ src/SPLGaussJordanPage.java
@@ -1,7 +1,13 @@
-public class SPLGaussJordanPage extends SPLPage
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class SPLGaussJordanPage extends Page
 {
     public SPLGaussJordanPage()
     {
-        super("../assets/spl-gaussjordan.png");
+        JLabel image = new JLabel(new ImageIcon("../assets/spl-gaussjordan.png"));
+        image.setBounds(0, 0, 700, 925);
+
+        page.add(image);
     }    
 }
