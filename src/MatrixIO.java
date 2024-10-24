e2969837c4758d001400e81ffffb0a073a98c435
Arlow5761 <arlow5761@gmail.com>
2024-10-24 08:44:36 +0700

    chore: resolve conflict between main and mainprogram branch

--- src/MatrixIO.java
+++ src/MatrixIO.java
@@ -41,7 +41,11 @@ public class MatrixIO
                 rowStringData = fileReader.nextLine().strip().split(" ");
                 rowData = new double[matrixData[0].length];
 
-                if (matrixData[0].length != rowStringData.length) return null;
+                if (matrixData[0].length != rowStringData.length)
+                {
+                    fileReader.close();
+                    return null;
+                }
 
                 for (int i = 0; i < rowData.length; i++)
                 {
@@ -52,6 +56,90 @@ public class MatrixIO
 
                 for (int i = 0; i < matrixData.length; i++)
                 {
+                    newMatrixData[i] = matrixData[i];
+                }
+
+                newMatrixData[matrixData.length] = rowData;
+
+                matrixData = newMatrixData;
+            }
+
+            fileReader.close();
+
+            return new Matrix(matrixData);
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    static public Matrix OpenImperfectMatrix()
+    {
+        JFileChooser fileChooser = new JFileChooser("../test");
+
+        int res = fileChooser.showOpenDialog(null);
+
+        if (res != JFileChooser.APPROVE_OPTION)
+        {
+            return null;
+        }
+
+        File file = fileChooser.getSelectedFile();
+
+        try
+        {
+            Scanner fileReader = new Scanner(file);
+
+            String[] rowStringData = fileReader.nextLine().strip().split(" ");
+
+            int maxRowSize = rowStringData.length;
+
+            double[] rowData = new double[maxRowSize];
+
+            for (int i = 0; i < rowData.length; i++)
+            {
+                rowData[i] = Double.parseDouble(rowStringData[i]);
+            }
+
+            double[][] matrixData = {rowData};
+
+            while (fileReader.hasNextLine())
+            {
+                rowStringData = fileReader.nextLine().strip().split(" ");
+
+                if (rowStringData.length > maxRowSize)
+                {
+                    maxRowSize = rowStringData.length;
+                }
+
+                rowData = new double[maxRowSize];
+
+                for (int i = 0; i < rowStringData.length; i++)
+                {
+                    rowData[i] = Double.parseDouble(rowStringData[i]);
+                }
+                for (int i = rowStringData.length; i < rowData.length; i++)
+                {
+                    rowData[i] = 0;
+                }
+
+                double[][] newMatrixData = new double[matrixData.length + 1][maxRowSize];
+
+                for (int i = 0; i < matrixData.length; i++)
+                {
+                    double[] newRowData = new double[maxRowSize];
+
+                    for (int j = 0; j < matrixData[i].length; j++)
+                    {
+                        newRowData[j] = rowData[j];
+                    }
+
+                    for (int j = matrixData[i].length; j < maxRowSize; j++)
+                    {
+                        newRowData[j] = 0;
+                    }
+
                     newMatrixData[i] = matrixData[i];
                 }
 
